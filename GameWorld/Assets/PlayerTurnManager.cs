2673e408fb745074db30ca9d6c4dbdb6eda1c09e
Raifa21 <79643312+Raifa21@users.noreply.github.com>
2024-07-09 23:36:56 +0700

    Created input reciever for button

--- GameWorld/Assets/PlayerTurnManager.cs
+++ GameWorld/Assets/PlayerTurnManager.cs
@@ -8,6 +8,12 @@ public class PlayerTurnManager : UdonSharpBehaviour
 {
     void Start()
     {
-        
+        Debug.Log("PlayerTurnManager started");
+    }
+
+    public void joinAsPlayer1()
+    {
+        // Set player 1
+        Debug.Log("Player 1 joined");
     }
 }

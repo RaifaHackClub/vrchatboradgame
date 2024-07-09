f35e4c25205b536ab9e7bec69ee668b8a9121daf
Raifa21 <79643312+Raifa21@users.noreply.github.com>
2024-07-09 15:45:13 +0700

    Altered Diceroll feature, and created turn manager

--- GameWorld/Assets/DiceRoll.cs
+++ GameWorld/Assets/DiceRoll.cs
@@ -6,54 +6,38 @@ using VRC.Udon;
 
 public class DiceRoll : UdonSharpBehaviour
 {
-    public float rollForce = 5f;
+    public float rollForce = 10f;
     public Transform[] diceFaces;
 
     private Rigidbody rb;
-
-    public void start()
+    public void Start()
     {
         rb = GetComponent<Rigidbody>();
         Debug.Log("DiceRoll script started");
-
-        if (rb == null)
-        {
-            Debug.LogError("DiceRoll requires a Rigidbody component on the same GameObject");
-        }
-
-        if (diceFaces.Length != 6)
-        {
-            Debug.LogError("DiceRoll requires 6 dice faces to be assigned");
-        }
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX;
+        //Rotate in random directions until interaction
+        RollDice();
 
     }
 
     public override void Interact()
     {
-        //unfreeze dice rotation and position
-        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        Debug.Log("DiceRoll script started");
-        RollDice();
+        //freeze dice rotation and position at the closest face
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+        Vector3 upwardsForce = Vector3.up * 3f;
+        rb.AddForce(upwardsForce, ForceMode.Impulse);
     }
 
     public void Update(){
-        if (GetComponent<Rigidbody>().IsSleeping())
-        {
-            transform.position = new Vector3(4f,1.5f,4f);
-            transform.
-            //freeze dice rotation and position
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        if(rb.IsSleeping()){
             CheckDiceResult();
         }
     }
 
     public void RollDice(){
-        Vector3 randomForce = new Vector3(Random.Range(-1f, 1f), Random.Range(1f, 1.5f), Random.Range(-1f, 1f)) * rollForce;
         Vector3 randomTorque = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * rollForce;
-
-
-        GetComponent<Rigidbody>().AddForce(randomForce, ForceMode.Impulse);
-        GetComponent<Rigidbody>().AddTorque(randomTorque, ForceMode.Impulse);
+        rb.AddTorque(randomTorque, ForceMode.Impulse);
     }
 
     public void CheckDiceResult(){
